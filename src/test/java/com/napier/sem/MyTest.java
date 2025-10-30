679d7493000aa3d07d4d2f7961fd25593f3f6d26
Dorian <40796561@napier.ac.uk>
2025-10-30 12:44:30 -0600

    unit testing

---
+++ src/test/java/com/napier/sem/MyTest.java
@@ -0,0 +1,20 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class MyTest
+{
+    @Test
+    public void unitTest()
+    {
+        assertEquals(5, 5);
+    }
+
+    @Test
+    void unitTest2()
+    {
+        assertEquals(5, 4);
+    }
+}
